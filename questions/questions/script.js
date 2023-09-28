003c38f2415d9ee8e6f1836b1603c9cebe27e0c5
yuki <136157812+yukimecox@users.noreply.github.com>
2023-09-28 12:31:03 -0700

    Add files via upload

---
+++ questions/questions/script.js
@@ -0,0 +1,34 @@
+// this will not be moved to askme.html for just this one example of creating a .js file.
+document.getElementById("write-something").addEventListener("submit", function(event) {
+    event.preventDefault(); // no data is being sent to the server (yet)
+
+    // apply background color for the "thank you" webpage
+    document.body.style.backgroundColor = "#EDD9D4";
+
+    // show thank you message
+    var container = document.querySelector(".container");   //target only child elements of parent (not descendants for querySelectorAll().)
+    container.innerHTML = "<h2>Thank you, Snacker. Your order will be shipped to your current address.</h2>";    //append the message
+
+    //create and append the image element
+    var image = document.createElement("img");
+    image.src = "../images/fillyLove.png";
+    container.appendChild(image);
+    var image2 = document.createElement("img");
+    image2.src = "../images/fillyLove.png";
+    container.appendChild(image2);
+    var image3 = document.createElement("img");
+    image3.src = "../images/fillyLove.png";
+    container.appendChild(image3);
+    var spaceImage = [image, image2, image3];           //create image array to space them apart
+
+    // create a div for the button
+    var buttonDiv = document.createElement("div");
+    var continueButton = document.createElement("button");
+    // add text to the button
+    continueButton.textContent = "Go Back";
+    // on click, go back to index.html (root). this can also mean: for some user interaction, where would you go in the website.
+    continueButton.addEventListener("click", function() { window.location.href = "../index.html"; });
+    // append each child to their parent to ensure the button works correctly
+    buttonDiv.appendChild(continueButton);
+    container.appendChild(buttonDiv);
+});
